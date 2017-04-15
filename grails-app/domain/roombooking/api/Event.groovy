91af637a2c379a4da56df0b7612e33691a685b54
Pongpon Tienthong <pongpon_tie@ascendinter.com>
2017-04-15 23:05:22 +0700

    change default language and validation

--- grails-app/domain/roombooking/api/Event.groovy
+++ grails-app/domain/roombooking/api/Event.groovy
@@ -11,8 +11,20 @@ class Event {
     String contactNumber
     String contactEmail
 
-
     static constraints = {
+        title nullable: false, blank: false
+        roomId nullable: false
+        start nullable: false
+        end nullable: false
+    }
 
+    def beforeValidate() {
+        if(!Room.exists(this.roomId)) {
+            this.errors.reject(
+                    'default.not.exist.message',
+                    ['roomId', 'Event', roomId] as Object[],
+                    '[Property [{0}] of class [{1}] with value [{2}] does not exist]'
+            )
+        }
     }
 }
