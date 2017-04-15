49e25a2cb0c0d836bbac76ffe99ff5bf66c90d28
Pongpon Tienthong <pongpon_tie@ascendinter.com>
2017-04-15 09:28:13 +0700

    bind roomId

--- grails-app/domain/roombooking/api/Event.groovy
+++ grails-app/domain/roombooking/api/Event.groovy
@@ -1,9 +1,15 @@
 package roombooking.api
 
+import grails.databinding.BindUsing
+
 class Event {
 
     String title
     String description
+
+    @BindUsing({ obj, source ->
+        [room: [id: source['roomId']]]
+    })
     Room room
     Date start
     Date end
