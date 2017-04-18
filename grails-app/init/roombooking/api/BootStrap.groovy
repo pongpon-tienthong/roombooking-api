28c25c937dae999db5893ef8b2de59902a98d748
Pongpon Tienthong <pongpon_tie@ascendinter.com>
2017-04-18 14:15:55 +0700

    change datasource config for dev env

--- grails-app/init/roombooking/api/BootStrap.groovy
+++ grails-app/init/roombooking/api/BootStrap.groovy
@@ -2,15 +2,9 @@ package roombooking.api
 
 class BootStrap {
 
-    def init = { servletContext ->
-        new Room(
-                name: 'Sanctaury Room',
-                description: 'The main worship hall',
-                capacity: 10,
-                status: 'active',
-                hexColor: '#444444'
-        ).save()
+    def init = {
     }
+
     def destroy = {
     }
 }
